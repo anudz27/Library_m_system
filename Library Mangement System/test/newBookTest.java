8df0a77c6a5900f628e3b8ebc4d8ddc8a423dc1c
Anuththara Dias <117101873+anudz27@users.noreply.github.com>
2025-01-19 21:39:29 +0530

    Update newBookTest.java

--- Library Mangement System/test/newBookTest.java
+++ Library Mangement System/test/newBookTest.java
@@ -10,10 +10,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author sithumwejesinghe
- */
+
 public class newBookTest {
     
     public newBookTest() {
